7138d492cebc013bb9fa54909312213b750f06d1
Hyuse <hyuse.dono@gmail.com>
2022-05-18 19:30:25 +0200

    Ajout script de récup des json + script de gestion des dialogues et voices line

---
+++ Tasu Shazia/Assets/Scripts/JSON/LoginClasses.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+[Serializable]
+public struct Dialogues
+{
+    public TextDialogues[] dialogues;
+}
+
+[Serializable]
+public struct TextDialogues
+{
+    public int id;
+    public string name;
+    public string color;
+    public string dialogue;
+    public string path;
+}
